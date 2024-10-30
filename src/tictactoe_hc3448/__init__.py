a301fdeb7f55b7e2d24de8be258ffd9d13c62343
HsiYuChen <cycn97@gmail.com>
2024-10-30 16:10:58 -0400

    init

--- src/tictactoe_hc3448/__init__.py
+++ src/tictactoe_hc3448/__init__.py
@@ -1,3 +1,5 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("tictactoe_hc3448")
+
+from .tictactoe_hc3448 import initialize_board, make_move, check_winner, reset_game
